24ec2d0191dbf2ef0b80e69a692939f5084c4a13
Benjamin Rome <benjamin.rome10@gmail.com>
2021-04-20 15:01:21 -0700

    Added Matplotlib w/graphical representaion of initial state

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,3 +5,10 @@ Created on Tue Apr 20 13:40:25 2021
 @author: benja
 """
 
+import matplotlib.pyplot as plt
+
+numbers = list(range(10))
+to_sort = [1,7,5,2,8,3,6,9,4,0]
+plt.bar(numbers, to_sort)
+
+plt.show()
