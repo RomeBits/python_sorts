eccb66ad49c11375b5a104aabfade643dc4641e0
Benjamin Rome <benjamin.rome10@gmail.com>
2021-04-25 15:09:00 -0700

    Finished bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,11 +4,42 @@ Created on Tue Apr 20 13:40:25 2021
 
 @author: benja
 """
-
 import matplotlib.pyplot as plt
+import random
 
-numbers = list(range(10))
-to_sort = [1,7,5,2,8,3,6,9,4,0]
-plt.bar(numbers, to_sort)
+N = 10
+X = list(range(N))
+to_sort = list(X)
 
+random.shuffle(to_sort)
+unsorted = list(to_sort)
+
+
+"""
+Loop through the array and swap elements in the wrong spot
+var: array, the array to loop through and sort 
+"""
+def bubble(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]: 
+            array[i], array[i + 1] = array[i + 1], array[i]
+
+
+"""
+Check if the array is sorted
+var: array, the array to check
+"""
+def check(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            return False
+    return True
+
+while(not check(to_sort)):
+    bubble(to_sort)
+    
+plt.subplot(211)
+plt.bar(X, unsorted)
+plt.subplot(212)
+plt.bar(X, to_sort)
 plt.show()
